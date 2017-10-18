9bd8b7c119c5674ec7fcf1ff485ce49d252474dc
Matthias <matthias.neuner@uibk.ac.at>
2017-10-18 13:21:22 +0200

    updated validation

--- validateEdelweiss.py
+++ validateEdelweiss.py
@@ -11,36 +11,47 @@ import os
 from fe.fecore import finitElementSimulation
 from fe.utils.inputfileparser import parseInputFile
 import numpy as np
+import argparse  
 
-testfile =  'test.inp' 
-referenceSolutionFile = 'U.ref'
 
-testfilesDir = os.path.join ( os.getcwd() , 'testfiles' )
-os.chdir(testfilesDir)
-testsDirs = next(os.walk('.'))[1]
+if __name__ == "__main__":    
+    parser = argparse.ArgumentParser(description='validation script for FE analyses')
+    parser.add_argument('--create', dest='create', action='store_true', help='create refernce solutions') 
+    args=parser.parse_args()
 
-for directory in testsDirs:
+    testfile =  'test.inp' 
+    referenceSolutionFile = 'U.ref'
     
-    os.chdir( os.path.join ( testfilesDir, directory ))
+    testfilesDir = os.path.join ( os.getcwd() , 'testfiles' )
+    os.chdir(testfilesDir)
+    testsDirs = next(os.walk('.'))[1]
     
-    if not os.path.exists(testfile):
-        continue
-    
-    UReference = np.loadtxt(referenceSolutionFile)
-    
-    inputFile = parseInputFile(testfile)
-    print('Test {:30}'.format(directory ), end='\r')
-    success, U, P, fieldOutputController = finitElementSimulation(inputFile, verbose = False)
-    
-    residual = U - UReference
-    
-    if ( np.max ( np.abs ( residual ) ) ) < 1e-6:
-        passed = True
-    else:
-        passed = False
-    
-    if passed:
-        print('Test {:30} PASSED'.format(directory))
-    else:
-        print('Test {:30} FAILED'.format(directory))
-    os.chdir('..')
+    for directory in testsDirs:
+        
+        os.chdir( os.path.join ( testfilesDir, directory ))
+        
+        if not os.path.exists(testfile):
+            continue
+        
+        inputFile = parseInputFile(testfile)
+        print('Test {:30}'.format(directory ), end='\r')
+        success, U, P, fieldOutputController = finitElementSimulation(inputFile, verbose = False)
+        
+        if not args.create:
+            
+            UReference = np.loadtxt(referenceSolutionFile)
+            residual = U - UReference
+            
+            if ( np.max ( np.abs ( residual ) ) ) < 1e-6:
+                passed = True
+            else:
+                passed = False
+            
+            if passed:
+                print('Test {:30} PASSED'.format(directory))
+            else:
+                print('Test {:30} FAILED'.format(directory))
+            os.chdir('..')
+            
+        else:
+            np.savetxt(referenceSolutionFile, U)
