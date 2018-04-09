f61bf3dbb450d61a5f6802b619d4a470a183834a
matthias <matthias.neuner@uibk.ac.at>
2018-04-09 20:07:32 +0200

    performance improvement

--- fe/utils/csrgenerator.pyx
+++ fe/utils/csrgenerator.pyx
@@ -35,16 +35,37 @@ cdef class CSRGenerator:
         indices = self.csrMatrix.indices
         indptr = self.csrMatrix.indptr
         
-        cdef int cooPairIdx, c
+        cdef int cooPairIdx, c, delta, nCols, base
         cdef long row, col
+        
         for cooPairIdx in range(len(I)):
             row = I [ cooPairIdx ]
             col = J [ cooPairIdx ]
-            for c in range( indptr[row], indptr[row+1] ):
-                colIdx = indices[ c ]
-                if colIdx == col:
+            
+            #binary search algorithm (can be improved based on lookup table, but seems to be sufficient for the moment)
+            base = indptr[row] 
+            nCols = indptr[row+1] - indptr[row]
+            delta = nCols / 2 
+            c = base + delta
+            while True:
+                colIdx = indices [ c  ]
+                if colIdx > col:
+                    delta = max ( delta >> 1 , 1 )
+                    c -= delta
+                elif colIdx < col:
+                    delta = max ( delta >> 1 , 1 )
+                    c += delta
+                else:
                     self.x [ cooPairIdx ] = c 
                     break
+            
+#            for c in range( indptr[row], indptr[row+1] ):
+#                colIdx = indices[ c ]
+#                if colIdx == col:
+#                    self.x [ cooPairIdx ] = c 
+#                    break
+                
+        
             
     def updateCSR(self, double[::1] V):
         """ get updated copies of the CSR matrix """
