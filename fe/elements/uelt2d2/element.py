3d60a8acb7399e6f2655c7ef73f757889aefe640
Matthias <matthias.neuner@uibk.ac.at>
2018-07-16 20:50:08 +0200

    added truss2, cleanup of testcases

---
+++ fe/elements/uelt2d2/element.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 14 19:22:23 2018
+
+@author: matthias
+"""
+
+from fe.elements.uelbaseelement.element import BaseElement
+
+
+class Element(BaseElement):
+    fields =                [["displacement", ], # node 1
+                             ["displacement", ], # node 2
+                             ]
+    
+    nNodes =                    2
+    nGaussPt =                  2
+    nDofPerEl =                 4
+    sizeKe =                    nDofPerEl * nDofPerEl
+    dofIndicesPermutation =     slice(0, 4)
+    ensightType =               "bar2"
+    uelIdentification =         "UelT2D2"
+    nStateVarsGaussPtSpecific = 12
+    
+    def __init__(self, nodes, elNumber):
+        super().__init__(nodes, elNumber, self.nGaussPt, self.nStateVarsGaussPtSpecific, self.uelIdentification)
