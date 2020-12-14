2f2ab49513f64ccc8b1db3e8ad7b1f52eb67e469
Matthias <matthias.neuner@uibk.ac.at>
2020-12-14 09:40:01 +0100

    changed install.py to setup.py; added improved setup instructions and mechanisms

---
+++ setup.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 21 14:23:14 2015
+
+@author: c8441141
+"""
+from distutils.core import setup, Extension
+from Cython.Build import cythonize
+from os.path import expanduser, join
+import numpy 
+import sys
+import os 
+
+directives = {
+        'boundscheck':            False,
+        'wraparound':             False,
+        'nonecheck' :             False,
+        'initializedcheck' :      False} 
+
+default_install_prefix = sys.prefix
+print("*"*80)
+print("EdelweissFE setup")
+print("System prefix: " + sys.prefix)
+print("*"*80)
+
+marmot_dir      = expanduser( os.environ.get('MARMOT_INSTALL_DIR',        default_install_prefix             ) )
+mkl_include     = expanduser( os.environ.get('MKL_INCLUDE_DIR',     join (default_install_prefix, 'include') ) )
+eigen_include   = expanduser( os.environ.get('EIGEN_INCLUDE_DIR',   join (default_install_prefix, 'include') ) )
+print("Marmot install directory (overwrite via environment var. MARMOT_INSTALL_DIR):")
+print(marmot_dir)
+print("MKL include directory (overwrite via environment var. MKL_INCLUDE_DIR):")
+print(mkl_include)
+print("Eigen directory (overwrite via environment var. EIGEN_INCLUDE_DIR):")
+print(eigen_include)
+print("*"*80)
+
+"""
+Build Extension for the MarmotElement base element, linked to the Marmot library
+"""
+extensions = [Extension("*",
+    sources = ["fe/elements/marmotelement/element.pyx"],
+    include_dirs=[join(marmot_dir, "include"), numpy.get_include()],
+    libraries= ['Marmot'],
+    library_dirs=         [join(marmot_dir, "lib") ] ,
+    runtime_library_dirs= [join(marmot_dir, "lib") ] ,
+    language='c++',)
+    ]  
+"""
+Auxiliary extension modules
+"""
+extensions += [Extension("*", ["fe/utils/elementresultcollector.pyx"],                                    
+    include_dirs=[numpy.get_include()],
+    language="c++",)]
+
+extensions += [Extension("*", ["fe/utils/csrgenerator.pyx"],                                    
+    include_dirs=[numpy.get_include()],
+    language="c++",)]
+
+"""
+Build The parallel NISTParallelMK2 solver with OpenMP
+"""
+extensions += [Extension("*",
+    sources = ["fe/solvers/nonlinearimplicitstaticparallelmk2.pyx"],
+    include_dirs=[numpy.get_include()] ,
+    language='c++',
+    extra_compile_args=['-fopenmp', '-Wno-maybe-uninitialized', ],
+    extra_link_args=['-fopenmp'],)
+    ]  
+
+"""
+Build The (old) parallel NISTParallel solver with OpenMP
+"""
+extensions += [Extension("*",
+    sources = ["fe/solvers/nonlinearimplicitstaticparallel.pyx"],
+    include_dirs=[numpy.get_include()] +  [join(marmot_dir, "include") ] ,
+    language='c++',
+    extra_compile_args=['-fopenmp', '-Wno-maybe-uninitialized', ],
+    extra_link_args=['-fopenmp'],)
+    ]  
+
+"""
+Build The Pardiso Interface
+"""
+extensions += [Extension("*",
+    sources = ["fe/linsolve/pardiso/pardiso.pyx", 'fe/linsolve/pardiso/pardisoInterface.cpp'],
+    include_dirs=[ numpy.get_include(), 
+        mkl_include, 
+        eigen_include,] ,
+    libraries= [ 'mkl_intel_thread', 'mkl_core', 'mkl_rt', 'iomp5', ],
+    language='c++',
+    )
+    ]  
+
+"""
+Compile!
+"""
+setup(ext_modules = cythonize(extensions,
+    compiler_directives=directives,
+    annotate=True,
+    language_level=3
+    ))
