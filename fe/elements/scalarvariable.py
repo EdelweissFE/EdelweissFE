16892149a0d464592735d487b649bd4a75c995c0
Matthias <matthias.neuner@uibk.ac.at>
2022-08-05 19:28:44 +0200

    added scalar variable

---
+++ fe/elements/scalarvariable.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#  ---------------------------------------------------------------------
+#
+#  _____    _      _              _         _____ _____
+# | ____|__| | ___| |_      _____(_)___ ___|  ___| ____|
+# |  _| / _` |/ _ \ \ \ /\ / / _ \ / __/ __| |_  |  _|
+# | |__| (_| |  __/ |\ V  V /  __/ \__ \__ \  _| | |___
+# |_____\__,_|\___|_| \_/\_/ \___|_|___/___/_|   |_____|
+#
+#
+#  Unit of Strength of Materials and Structural Analysis
+#  University of Innsbruck,
+#  2017 - today
+#
+#  Matthias Neuner matthias.neuner@uibk.ac.at
+#
+#  This file is part of EdelweissFE.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2.1 of the License, or (at your option) any later version.
+#
+#  The full text of the license can be found in the file LICENSE.md at
+#  the top level directory of EdelweissFE.
+#  ---------------------------------------------------------------------
+"""
+Created on Fri Jan 27 19:53:45 2017
+
+@author: Matthias Neuner
+"""
+
+
+class ScalarVariable:
+    """Base class for a scalar variable, not tied to a spatial position
+    currently nothing more than a container its index.
+    """
+
+    def __init__(
+        self,
+    ):
+
+        self.index = None
