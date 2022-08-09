978174dd37b5d1e7f3ce845b3a5df9f40e0434d5
Matthias Neuner <matthias.neuner@uibk.ac.at>
2022-08-09 10:02:12 -0700

    added find closest node functionality

---
+++ fe/generators/findclosestnode.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#  ---------------------------------------------------------------------
+#
+#  _____    _      _              _         _____ _____
+# | ____|__| | ___| |_      _____(_)___ ___|  ___| ____|
+# |  _| / _` |/ _ \ \ \ /\ / / _ \ / __/ __| |_  |  _|
+# | |__| (_| |  __/ |\ V  V /  __/ \__ \__ \  _| | |___
+# |_____\__,_|\___|_| \_/\_/ \___|_|___/___/_|   |_____|
+#
+#
+#  Unit of Strength of Materials and Structural Analysis
+#  University of Innsbruck,
+#  2017 - today
+#
+#  Matthias Neuner matthias.neuner@uibk.ac.at
+#
+#  This file is part of EdelweissFE.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2.1 of the License, or (at your option) any later version.
+#
+#  The full text of the license can be found in the file LICENSE.md at
+#  the top level directory of EdelweissFE.
+#  ---------------------------------------------------------------------
+# Created on Tue Aug 9 15:41:51 2022
+
+# @author: Matthias Neuner
+"""
+Find the node closest to a given spatial position, and store it in an existing or new node set.
+"""
+
+documentation = {"location": "The location of the node", "storeIn": "Store in this node set"}
+
+from fe.variables.node import Node
+from fe.utils.misc import stringDict
+from fe.journal.journal import Journal
+
+from fe.utils.exceptions import WrongDomain
+
+import numpy as np
+
+
+def generateModelData(generatorDefinition: dict, modelInfo: dict, journal: Journal):
+
+    options = generatorDefinition["data"]
+    options = stringDict([e for l in options for e in l])
+
+    loc = np.fromstring(options["location"], sep=",", dtype=np.float)
+
+    if len(loc) != modelInfo["domainSize"]:
+        raise WrongDomain("Spatial dimension of specified location does not match model dimension")
+
+    allNodes = np.asarray([n.coordinates for n in modelInfo["nodes"].values()])
+
+    differenceNorm = np.linalg.norm(allNodes - loc, axis=1)
+
+    indexClosest = differenceNorm.argmin()
+
+    closestNodeID = list(modelInfo["nodes"].keys())[indexClosest]
+
+    if options["storeIn"] in modelInfo["nodeSets"]:
+        modelInfo["nodeSets"][options["storeIn"]].append(closestNodeID)
+    else:
+        modelInfo["nodeSets"][options["storeIn"]] = [
+            closestNodeID,
+        ]
+
+    return modelInfo
