1aede1fc70d2c4987a457125058be51f7a9797c5
Matthias Neuner <matthias.neuner@uibk.ac.at>
2022-09-04 09:36:00 -0700

    fix in type checking

--- fe/utils/elementresultcollector.pyx
+++ fe/utils/elementresultcollector.pyx
@@ -39,7 +39,7 @@ cdef class ElementResultCollector:
     cdef double[:, :, ::1] res_
     cdef double** resultPointers
     
-    def __init__(self, elements:list, quadraturePoints:slice, result:str):
+    def __init__(self, elements:list, quadraturePoints:range, result:str):
         """
         A cdef class for collecting element results (by using the permanent results pointer (i.e., a numpy array) 
         in large array of all elements and all quadrature points.
