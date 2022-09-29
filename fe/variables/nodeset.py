8d842b858fe9033b9ddb011b99e4eff5be920f52
Dummer Alexander <alexander.dummer@student.uibk.ac.at>
2022-09-29 09:35:36 +0200

    add missing nodeset.py

---
+++ fe/variables/nodeset.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#  ---------------------------------------------------------------------
+#
+#  _____    _      _              _         _____ _____
+# | ____|__| | ___| |_      _____(_)___ ___|  ___| ____|
+# |  _| / _` |/ _ \ \ \ /\ / / _ \ / __/ __| |_  |  _|
+# | |__| (_| |  __/ |\ V  V /  __/ \__ \__ \  _| | |___
+# |_____\__,_|\___|_| \_/\_/ \___|_|___/___/_|   |_____|
+#
+#
+#  Unit of Strength of Materials and Structural Analysis
+#  University of Innsbruck,
+#  2017 - today
+#
+#  Alexander Dummer alexander.dummer@uibk.ac.at
+#
+#  This file is part of EdelweissFE.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2.1 of the License, or (at your option) any later version.
+#
+#  The full text of the license can be found in the file LICENSE.md at
+#  the top level directory of EdelweissFE.
+#  ---------------------------------------------------------------------
+
+# @author: Alexander Dummer
+
+from fe.variables.orderedset import OrderedSet
+
+
+class NodeSet(OrderedSet):
+    """A basic node set.
+    It has a label, and a list containing the unique nodes.
+
+    Parameters
+    ----------
+    label
+        The unique label for this element set.
+    nodes
+        A list of nodes.
+    """
+
+    def __init__(
+        self,
+        label: str,
+        nodes: list,
+    ):
+
+        super().__init__(label, nodes)
