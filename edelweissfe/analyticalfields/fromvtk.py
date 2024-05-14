404882e57d66d2b22fa321eb642095125ca05818
Paul Hofer <paul.hofer@uibk.ac.at>
2024-05-14 17:41:00 +0200

    AnalyticalFields FromVtk

---
+++ edelweissfe/analyticalfields/fromvtk.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#  ---------------------------------------------------------------------
+#
+#  _____    _      _              _         _____ _____
+# | ____|__| | ___| |_      _____(_)___ ___|  ___| ____|
+# |  _| / _` |/ _ \ \ \ /\ / / _ \ / __/ __| |_  |  _|
+# | |__| (_| |  __/ |\ V  V /  __/ \__ \__ \  _| | |___
+# |_____\__,_|\___|_| \_/\_/ \___|_|___/___/_|   |_____|
+#
+#
+#  Unit of Strength of Materials and Structural Analysis
+#  University of Innsbruck,
+#  2017 - today
+#
+#  Paul Hofer Paul.Hofer@uibk.ac.at
+#
+#  This file is part of EdelweissFE.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2.1 of the License, or (at your option) any later version.
+#
+#  The full text of the license can be found in the file LICENSE.md at
+#  the top level directory of EdelweissFE.
+#  ---------------------------------------------------------------------
+"""Use PyVista to interpolate from vtk data.
+"""
+
+documentation = {
+    "file": "path to database file",
+    "result": "result name in database (optional if database contains only one dataset)",
+}
+
+import numpy as np
+
+# import sympy as sp
+# import gstools
+from edelweissfe.utils.misc import convertLinesToStringDictionary
+
+# from edelweissfe.utils.math import createFunction
+from edelweissfe.analyticalfields.base.analyticalfieldbase import (
+    AnalyticalField as AnalyticalFieldBase,
+)
+
+# from inspect import signature
+
+import pyvista
+
+
+class AnalyticalField(AnalyticalFieldBase):
+    """ """
+
+    def __init__(self, name, data, model):
+        self.name = name
+        self.type = "fromVtk"
+
+        options = convertLinesToStringDictionary(data)
+
+        self.domainSize = model.domainSize
+
+        file = options["file"]
+
+        reader = pyvista.get_reader(file)
+        self.data = reader.read()
+
+        result = options.get("result")
+        availableResults = self.data.array_names
+
+        try:
+            assert len(availableResults) > 0
+        except AssertionError:
+            raise AssertionError("Database does not contain at least one result.")
+
+        if not result:
+            if len(availableResults) == 1:
+                result = self.data.array_names[0]
+            else:
+                raise ValueError("Database contains multiple results. Specify result with option result=...")
+        else:
+            if result not in availableResults:
+                raise KeyError(f"Specified result '{result}' not available. Available results: {availableResults}")
+
+        self.result = result
+
+        return
+
+    def evaluateAtCoordinates(self, coords):
+        coords = np.array(coords)
+
+        interpolatedData = pyvista.PointSet(coords).sample(self.data)
+        interpolatedResult = interpolatedData[self.result]
+
+        return np.expand_dims(interpolatedResult, 1)
