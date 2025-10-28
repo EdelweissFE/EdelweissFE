5a66644879681b14190e42cc3490d5db1c467cdd
Alexander Dummer <alexander.dummer@uibk.ac.at>
2025-10-28 11:05:55 -0600

    format

--- edelweissfe/constraints/rigidbody.py
+++ edelweissfe/constraints/rigidbody.py
@@ -108,7 +108,7 @@ class Constraint(ConstraintBase):
 
         self.nRot = 3
 
-        self._reactions = np.zeros( self.nRot + self.nDim )
+        self._reactions = np.zeros(self.nRot + self.nDim)
 
     @property
     def nodes(self) -> list:
@@ -242,8 +242,8 @@ class Constraint(ConstraintBase):
 
         # start and end of Lambda in P
         L0, LF = nU, nU + nDim
-        
-        self._reactions.fill( 0.0 )
+
+        self._reactions.fill(0.0)
 
         for i in range(nSlaves):
             d0 = self.distancesSlaveNodeRP[i]
@@ -279,6 +279,6 @@ class Constraint(ConstraintBase):
 
             L0 += nDim
             LF += nDim
-            
-            self._reactions[0:self.nDim] += Lambda
-            self._reactions[self.nDim:] += np.cross( T @ d0, Lambda )
+
+            self._reactions[0 : self.nDim] += Lambda
+            self._reactions[self.nDim :] += np.cross(T @ d0, Lambda)
